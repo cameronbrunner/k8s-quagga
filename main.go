cc9b17ddf97c44f2f5dddbc7f6d4d8cc274fcbd6
Cameron Brunner <brunner@univa.com>
2015-11-23 17:58:30 -0600

    Add godep support

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "os"
-//  "fmt"
-  "path"
-  "text/template"
-  "github.com/codegangsta/cli"
+	"os"
+	//  "fmt"
+	"github.com/k8s-quagga/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"path"
+	"text/template"
 )
 
 type ospfconfig struct {
-  Password string
-  Interface string
-  RouterId string
-  HomeNet string
-//  PortalNet string
-  ContainerNet string
+	Password  string
+	Interface string
+	RouterId  string
+	HomeNet   string
+	//  PortalNet string
+	ContainerNet string
 }
 
 type zebraconfig struct {
-  Password string
-  PortalNet string
-  PortalGw string
+	Password  string
+	PortalNet string
+	PortalGw  string
 }
 
 const zebraTemplate = `password {{.Password}}
@@ -48,108 +48,107 @@ router ospf
 `
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-  app := cli.NewApp()
-  app.Version = "0.0.4"
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name: "output",
-      Value: "/etc/quagga",
-      Usage: "Directory to output config to",
-      EnvVar: "K8S_QUAGGA_OUTPUT",
-    },
-    cli.StringFlag{
-      Name: "password",
-      Value: "changeme",
-      Usage: "Password for authorization",
-      EnvVar: "K8S_QUAGGA_PASSWORD",
-    },
-  }
-  app.Commands = []cli.Command{
-    {
-      Name: "ospfd",
-      Usage: "output ospfd config",
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "Interface",
-          Value: "eth0",
-          Usage: "The interface to announce on",
-        },
-        cli.StringFlag{
-          Name: "RouterId",
-          Value: "10.0.0.1",
-          Usage: "Router ID to announce",
-        },
-        cli.StringFlag{
-          Name: "ContainerNet",
-          Value: "10.2.0.0/24",
-          Usage: "Container Network CIDR",
-        },
-//        cli.StringFlag{
-//          Name: "PortalNet",
-//          Value: "10.2.0.0/24",
-//          Usage: "Portal Network CIDR",
-//        },
-        cli.StringFlag{
-          Name: "HomeNet",
-          Value: "10.0.1.0/24",
-          Usage: "Home Network CIDR",
-        },
-      },
-      Action: func(c *cli.Context) {
-        config := ospfconfig{
-          Password: c.GlobalString("password"),
-          Interface: c.String("Interface"),
-          RouterId: c.String("RouterId"),
-          HomeNet: c.String("HomeNet"),
-//          PortalNet: c.String("PortalNet"),
-          ContainerNet: c.String("ContainerNet"),
-        }
-        f, err := os.Create(path.Join(c.GlobalString("output"), "ospfd.conf"))
-        check(err)
-        defer f.Close()
-        t, err := template.New("config").Parse(ospfdTemplate)
-        check(err)
-        err = t.Execute(f, config)
-        check(err)
-      },
-    },
-    {
-      Name: "zebra",
-      Usage: "output zebra config",
-      Flags: []cli.Flag {
-        cli.StringFlag{
-          Name: "PortalNet",
-          Value: "10.2.0.0/24",
-          Usage: "Portal Network CIDR",
-        },
-        cli.StringFlag{
-          Name: "PortalGw",
-          Value: "10.0.1.4",
-          Usage: "Portal Network Gateway",
-        },
-      },
-      Action: func(c *cli.Context) {
-        config := zebraconfig{
-          Password: c.GlobalString("password"),
-          PortalNet: c.String("PortalNet"),
-          PortalGw: c.String("PortalGw"),
-        }
-        f, err := os.Create(path.Join(c.GlobalString("output"), "zebra.conf"))
-        check(err)
-        defer f.Close()
-        t, err := template.New("config").Parse(zebraTemplate)
-        check(err)
-        err = t.Execute(f, config)
-        check(err)
-      },
-    },
-  }
-  app.Run(os.Args)
+	app := cli.NewApp()
+	app.Version = "0.0.4"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "output",
+			Value:  "/etc/quagga",
+			Usage:  "Directory to output config to",
+			EnvVar: "K8S_QUAGGA_OUTPUT",
+		},
+		cli.StringFlag{
+			Name:   "password",
+			Value:  "changeme",
+			Usage:  "Password for authorization",
+			EnvVar: "K8S_QUAGGA_PASSWORD",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "ospfd",
+			Usage: "output ospfd config",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "Interface",
+					Value: "eth0",
+					Usage: "The interface to announce on",
+				},
+				cli.StringFlag{
+					Name:  "RouterId",
+					Value: "10.0.0.1",
+					Usage: "Router ID to announce",
+				},
+				cli.StringFlag{
+					Name:  "ContainerNet",
+					Value: "10.2.0.0/24",
+					Usage: "Container Network CIDR",
+				},
+				//        cli.StringFlag{
+				//          Name: "PortalNet",
+				//          Value: "10.2.0.0/24",
+				//          Usage: "Portal Network CIDR",
+				//        },
+				cli.StringFlag{
+					Name:  "HomeNet",
+					Value: "10.0.1.0/24",
+					Usage: "Home Network CIDR",
+				},
+			},
+			Action: func(c *cli.Context) {
+				config := ospfconfig{
+					Password:  c.GlobalString("password"),
+					Interface: c.String("Interface"),
+					RouterId:  c.String("RouterId"),
+					HomeNet:   c.String("HomeNet"),
+					//          PortalNet: c.String("PortalNet"),
+					ContainerNet: c.String("ContainerNet"),
+				}
+				f, err := os.Create(path.Join(c.GlobalString("output"), "ospfd.conf"))
+				check(err)
+				defer f.Close()
+				t, err := template.New("config").Parse(ospfdTemplate)
+				check(err)
+				err = t.Execute(f, config)
+				check(err)
+			},
+		},
+		{
+			Name:  "zebra",
+			Usage: "output zebra config",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "PortalNet",
+					Value: "10.2.0.0/24",
+					Usage: "Portal Network CIDR",
+				},
+				cli.StringFlag{
+					Name:  "PortalGw",
+					Value: "10.0.1.4",
+					Usage: "Portal Network Gateway",
+				},
+			},
+			Action: func(c *cli.Context) {
+				config := zebraconfig{
+					Password:  c.GlobalString("password"),
+					PortalNet: c.String("PortalNet"),
+					PortalGw:  c.String("PortalGw"),
+				}
+				f, err := os.Create(path.Join(c.GlobalString("output"), "zebra.conf"))
+				check(err)
+				defer f.Close()
+				t, err := template.New("config").Parse(zebraTemplate)
+				check(err)
+				err = t.Execute(f, config)
+				check(err)
+			},
+		},
+	}
+	app.Run(os.Args)
 }
-
